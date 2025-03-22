f19a06acc659d6fa2b8b899fc83cc2f27a6c21a2
Sebilebi <ivanbollz25@gmail.com>
2025-03-22 11:14:44 +0100

    Migrar todo el proyecto de MAUI a WPF con BlazorWebView

--- PKure/App.xaml.cs
+++ PKure/App.xaml.cs
@@ -1,15 +1,14 @@
-﻿namespace PKure
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PKure
 {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
     public partial class App : Application
     {
-        public App()
-        {
-            InitializeComponent();
-        }
-
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new MainPage()) { Title = "PKure" };
-        }
     }
+
 }
---
+++ PKure/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+
+namespace PKure
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+
+            InitializeComponent();
+        }
+    }
+}
