18aa26a0a8d40e825e53eb86c4e0bebc1b6cba5f
Sebilebi <ivanbollz25@gmail.com>
2025-04-01 21:22:48 +0200

    Creada clase para mappear datos de la pokeapi v2

---
+++ PKure/Mapping/PokemonMapper.cs
@@ -0,0 +1,22 @@
+﻿using PKure.Models;
+
+namespace PKure.Mapping
+{
+    public static class PokemonMapper
+    {
+        public static PokemonViewModel MapToViewModel(Pokemon apiPokemon)
+        {
+            return new PokemonViewModel
+            {
+                Id = apiPokemon.Id,
+                Name = char.ToUpper(apiPokemon.Name[0]) + apiPokemon.Name.Substring(1),
+                ImageUrl = apiPokemon.ImageUrl,
+                Type = (apiPokemon.Types != null && apiPokemon.Types.Any())
+                        ? string.Join(" / ", apiPokemon.Types
+                            .OrderBy(t => t.Slot)
+                            .Select(t => char.ToUpper(t.Type.Name[0]) + t.Type.Name.Substring(1)))
+                        : "N/A"
+            };
+        }
+    }
+}
