70150de5cfd33a2748614fdd956ab203dc4937d9
Sebilebi <ivanbollz25@gmail.com>
2025-04-01 21:20:35 +0200

    Creado endpoints:
    -Obtener los primeros 20 pokemons
    -Obtener un pokemon por id

---
+++ PKure/Services/PokeApiEndpoints.cs
@@ -0,0 +1,14 @@
+﻿namespace PKure.Services
+{
+    public static class PokeApiEndpoints
+    {
+        public const string BaseUrl = "https://pokeapi.co/api/v2/";
+
+        // Endpoint para obtener los primeros 20 Pokémon.
+        public const string PokemonList = BaseUrl + "pokemon/?limit=20";
+
+        // Método para obtener el endpoint de detalle de un Pokémon por nombre o id.
+        public static string GetPokemonDetailUrl(string nameOrId)
+            => $"{BaseUrl}pokemon/{nameOrId}";
+    }
+}
