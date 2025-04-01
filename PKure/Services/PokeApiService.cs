1dd5e2c7b36a6a3b152318dfee3086e92ca6cd41
Sebilebi <ivanbollz25@gmail.com>
2025-04-01 21:24:27 +0200

    Creado servicio para obtener|mapear los 20 primeros pokemons

---
+++ PKure/Services/PokeApiService.cs
@@ -0,0 +1,32 @@
+﻿using PKure.Mapping;
+using PKure.Models;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+namespace PKure.Services
+{
+    public class PokeApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public PokeApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        // Obtiene los primeros 20 Pokémon y los mapea a PokemonViewModel.
+        public async Task<List<PokemonViewModel>> GetFirst20PokemonAsync()
+        {
+            var listResponse = await _httpClient.GetFromJsonAsync<PokemonListResponse>(PokeApiEndpoints.PokemonList);
+            if (listResponse?.Results == null)
+                return new List<PokemonViewModel>();
+
+            var detailTasks = listResponse.Results
+                .Select(item => _httpClient.GetFromJsonAsync<Pokemon>(item.Url))
+                .ToArray();
+
+            var apiPokemons = await Task.WhenAll(detailTasks);
+            return apiPokemons.Select(p => PokemonMapper.MapToViewModel(p)).ToList();
+        }
+    }
+}
