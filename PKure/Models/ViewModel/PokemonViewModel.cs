d5e3dcfccb16b73b82995dbdf7dc602bc369839e
Sebilebi <ivanbollz25@gmail.com>
2025-04-01 22:23:53 +0200

    Redimensiado la ventana de la app

--- PKure/Models/ViewModel/PokemonViewModel.cs
+++ PKure/Models/ViewModel/PokemonViewModel.cs
@@ -4,8 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        // Cadena que une los tipos, por ejemplo "Grass / Poison"
-        public string Type { get; set; }
+        public string Type { get; set; } // Cadena que une los tipos, por ejemplo "Grass / Poison"
         public string ImageUrl { get; set; }
     }
 }
